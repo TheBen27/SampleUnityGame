54356be5a8e0afd3064a84da2ac5b67f1c490f71
Ben Powell <bdpowell@email.wm.edu>
2018-01-27 17:11:13 -0500

    add shark object that reads in and acts out position and orientation data

---
+++ Assets/Scripts/MinorGridLineManagement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinorGridLineManagement : MonoBehaviour {
+
+    public GameObject gridLineObject;
+
+    public int numGridLines = 10;
+    public float gridLineSpacing = 1.0f;
+
+	// Create our minor grid lines
+	void Start () {
+        Quaternion horRotation = Quaternion.identity;
+        Quaternion verRotation = Quaternion.AngleAxis(90.0f, Vector3.up);
+        float extent = numGridLines * gridLineSpacing / 2.0f;
+
+        // Horizontal grid lines along the Z axis
+        for (float z = -extent; z < extent; z += gridLineSpacing)
+        {
+            Vector3 pos = new Vector3(0.0f, 0.0f, z);
+            Instantiate(gridLineObject, pos, horRotation, transform);
+        }
+
+        // Vertical grid lines along the X axis
+        for (float x = -extent; x < extent; x += gridLineSpacing)
+        {
+            Vector3 pos = new Vector3(x, 0.0f, 0.0f);
+            Instantiate(gridLineObject, pos, verRotation, transform);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SimpleSharkMover.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/*
+ * Hacked-together script that loads in tmp-data and tmp-eas, then moves it
+ * at 24 FPS. Data is loaded and integrated at startup, so expect a small delay at Start.
+ */
+public class SimpleSharkMover : MonoBehaviour {
+
+    /*
+     * Two CSV files, the first containing accelerometer data in the first
+     * three columns, the second containing orientation data.
+     */
+    public TextAsset movementData;
+    public TextAsset orientationData;
+
+    public float inputSampleRate = 20.0f;
+    public float startTime = 20.0f;
+
+    private List<Vector3> position;
+    private List<Vector3> orientation;
+
+    private float currentTime;
+
+	// Use this for initialization
+	void Start () {
+        currentTime = startTime;
+
+        List<Vector3> acceleration = ParseFloatingCSV(movementData.text, 1);
+        position = AccelToPos(acceleration);
+        orientation = ParseFloatingCSV(orientationData.text, 2);
+
+        Assert.AreEqual(position.Count, orientation.Count,
+            "Position and orientation lists do not have the same number of elements." +
+            " (" + position.Count + " != " + orientation.Count + ")");
+        Assert.AreNotEqual(position.Count, 0,
+            "Lists are empty or could not be read.");
+    }
+	
+	void Update () {
+        if (position.Count == 0)
+            return;
+
+        currentTime %= (position.Count / inputSampleRate);
+        float currentFrame = currentTime * inputSampleRate;
+
+        int frame1 = (int) currentFrame;
+        int frame2 = frame1 + 1;
+        float lerpAmount = currentFrame - frame1;
+        
+        if (lerpAmount < 0.0001f || frame2 >= position.Count)
+        {
+            transform.localPosition = position[frame1];
+            transform.localEulerAngles = orientation[frame1];
+        } else
+        {
+            transform.localPosition = Vector3.Lerp(position[frame1], position[frame2], lerpAmount);
+            transform.localEulerAngles = Vector3.Lerp(orientation[frame1], orientation[frame2], lerpAmount);
+        }
+
+        currentTime += Time.deltaTime;
+	}
+
+    private List<Vector3> ParseFloatingCSV(string csv, int rowsToSkip)
+    {
+        string[] lines = csv.Split('\n');
+        List<Vector3> parsed = new List<Vector3>(lines.Length);
+        for (int i=rowsToSkip; i<lines.Length; i++)
+        {
+            string line = lines[i];
+            string[] floats = line.Split(',');
+            if (floats[0] == "")
+            {
+                continue;
+            }
+            Assert.IsTrue(floats.Length >= 3, "Line doesn't have enough elements");
+            parsed.Add(new Vector3(
+                float.Parse(floats[0]),
+                float.Parse(floats[1]),
+                float.Parse(floats[2])
+            ));
+
+        }
+        return parsed;
+    }
+
+    /*
+     * Double-integrate accelerometer data into position data,
+     * taking into account sample rate and whatnot
+     */
+    private List<Vector3> AccelToPos(List<Vector3> accel)
+    {
+        return accel;
+    }
+
+    
+}
